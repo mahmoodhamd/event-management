2be30f521e2384d6ac376a3f16df26f8e53f1a96
mahmoodhamd <116299051+mahmoodhamd@users.noreply.github.com>
2024-01-07 17:53:17 +0500

    seeder and factory classes added

--- database/migrations/2024_01_03_173309_create_events_table.php
+++ database/migrations/2024_01_03_173309_create_events_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Models\User;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -16,8 +17,8 @@ return new class extends Migration
            $table->foreignIDFor(User::class);
            $table->string('name');
            $table->text('description')->nullable();
-           $table->dataTime('start_time');
-           $table->dataTime('end_time');
+           $table->dateTime('start_time');
+           $table->dateTime('end_time');
             $table->timestamps();
         });
     }
